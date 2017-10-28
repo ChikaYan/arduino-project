982c3b15786d4c518a4e736d8978b4f69f34527e
ChikamaYan <walterwuyan@gmail.com>
2017-10-28 09:53:50 +0100

    add tilt-sensor

--- arduinoProgram/tiltSensor/tiltSensor/tiltSensor.ino
+++ arduinoProgram/tiltSensor/tiltSensor/tiltSensor.ino
@@ -4,35 +4,26 @@ int rightInPin = 9;
 int currentStat = 0; //0 means untilted, 1 means left-tilted and 2 means right-tilted
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(leftInPin, INPUT);
   pinMode(rightInPin, INPUT);
-  Serial.println("Untilted");
+  Serial.println("Untilted");//init status
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val;
 
-  
-//  val = digitalRead(rightInPin); //0 means untilted, 1 means tilted
-//  Serial.println(val);
-//  delay(500);
-  
+  //check if status has changed
   if ( digitalRead(leftInPin) == 1 && currentStat != 1){
-    //stat changed to left-tilted
     currentStat = 1;
-    Serial.println("Left-tilted");
+    Serial.println(currentStat); //left-titlted
   }else if(digitalRead(rightInPin) == 1 && currentStat != 2){
-    //stat changed to right-tilted
     currentStat = 2;
-    Serial.println("Right-tilted");
+    Serial.println(currentStat);//right-tilted
   }else if (digitalRead(leftInPin) == 0 &&digitalRead(rightInPin) == 0 && currentStat != 0){
-    //stat changed to untilted
     currentStat = 0;
-    Serial.println("Untilted");
+    Serial.println(currentStat);//untilted
   }
-  delay(100);
   
+  delay(100);
+  //delay in case of massive output
 }
