e309204d6d5a648cb9f48697d2f938f555bf5565
Walter Wu <walterwuyan@gmail.com>
2017-10-30 08:57:33 +0000

    changed directory names

--- c-program/receive-output/test.c
+++ c-program/receive-output/test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int a=0;
+    int a = 0;
 
     while (1) {
         scanf("%d ", &a);
         printf("The number read was: %d\n", a);
-        if (a==0) break;
-}
-return 0;
+        if (a == 0) break;
+    }
+    return 0;
 }
--- cProgram/receiveOutput/cmake-build-debug/CMakeFiles/receiveOutputTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for receiveOutputTest.
-# This may be replaced when dependencies are built.
